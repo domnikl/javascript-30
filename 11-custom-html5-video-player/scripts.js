a66c32d84e6b27c6e38613252ef5931ca2c9af22
Dominik Liebler <dominik.liebler@flyeralarm.com>
2021-04-16 21:45:18 +0200

    add fullscreen functionality

--- 11-custom-html5-video-player/scripts.js
+++ 11-custom-html5-video-player/scripts.js
@@ -6,6 +6,7 @@ const progressBar = player.querySelector(".progress__filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
 const ranges = player.querySelectorAll(".player__slider");
+const fullscreen = player.querySelector(".fullscreen");
 
 /* build our functions*/
 function togglePlay() {
@@ -34,7 +35,16 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+function toggleFullscreen() {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+  } else {
+    video.requestFullscreen();
+  }
+}
+
 /* hook up the event listeners */
+video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 video.addEventListener("timeupdate", handleProgress);
@@ -53,3 +63,19 @@ ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
 ranges.forEach((range) =>
   range.addEventListener("mousemove", handleRangeUpdate)
 );
+
+fullscreen.addEventListener("click", toggleFullscreen);
+
+// keyboard shortcuts
+document.addEventListener("keyup", (e) => {
+  switch (e.keyCode) {
+    case 32: // space
+      togglePlay();
+      break;
+    case 122: // F11
+      toggleFullscreen();
+      break;
+    default:
+      console.log(e);
+  }
+});
